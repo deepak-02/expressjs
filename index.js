11f2a74012623b7371f2eb9ca886b5d24d95c18e
Deepak <104752197+deepak-02@users.noreply.github.com>
2023-04-22 22:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,9 @@ app.use(
 const db = require("./app/models");
 const Role = db.role;
 
+//mongosh "mongodb://mongo:2MEztr1AYzClTgXfM6Yx@containers-us-west-176.railway.app:5791"
 db.mongoose
-    .connect(`mongodb://mongo:dYyr6LcPDr2aOojksjfo@containers-us-west-75.railway.app:5520`, {
+    .connect(`mongodb://mongo:2MEztr1AYzClTgXfM6Yx@containers-us-west-176.railway.app:5791`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
