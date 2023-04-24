9823493d42c9df0a5eef14b9b4497ddc8fb810aa
Deepak <104752197+deepak-02@users.noreply.github.com>
2023-04-24 12:06:46 +0530

    new fix

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -3,46 +3,40 @@ const ROLES = db.ROLES;
 const User = db.user;
 
 checkDuplicateUsernameOrEmail = (req, res, next) => {
+    // Username
     User.findOne({
-        username: req.body.username
-    }).then(user => {
-        if (user) {
-            res.status(400).send({ message: "Failed! Username is already in use!" });
-            return;
+        email: req.body.email
+    }).then((user) => {
+            //if succeded do this block of code
+        if (user){
+            return res.status(400).send({ message: "Failed! Username is already in use!" });
         }
+        next();
 
-        User.findOne({
-            email: req.body.email
-        }).then(user => {
-            if (user) {
-                res.status(400).send({ message: "Failed! Email is already in use!" });
-                return;
-            }
-
-            next();
-        }).catch(err => {
-            res.status(500).send({ message: err });
-        });
-    }).catch(err => {
-        res.status(500).send({ message: err });
+        }).catch((err) => {
+        //catch error
+       return res.status(500).send({ message: err });
     });
+
 };
 
-
-const checkRolesExisted = (req, res, next) => {
-  if (req.body.roles) {
-    for (let i = 0; i < req.body.roles.length; i++) {
-      if (!ROLES.includes(req.body.roles[i])) {
-        return res.status(400).send({ message: `Failed! Role ${req.body.roles[i]} does not exist!` });
-      }
+checkRolesExisted = (req, res, next) => {
+    if (req.body.roles) {
+        for (let i = 0; i < req.body.roles.length; i++) {
+            if (!ROLES.includes(req.body.roles[i])) {
+                res.status(400).send({
+                    message: `Failed! Role ${req.body.roles[i]} does not exist!`
+                });
+                return;
+            }
+        }
     }
-  }
-  next();
+    next();
 };
 
 const verifySignUp = {
-  checkDuplicateUsernameOrEmail,
-  checkRolesExisted
+    checkDuplicateUsernameOrEmail,
+    checkRolesExisted
 };
 
 module.exports = verifySignUp;
