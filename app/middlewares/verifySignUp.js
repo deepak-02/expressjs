91e931bc07faafa958b53ef60daee8e2c2005e9e
Deepak <104752197+deepak-02@users.noreply.github.com>
2023-04-24 11:52:35 +0530

    Update verifySignUp.js

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -2,25 +2,32 @@ const db = require("../models");
 const ROLES = db.ROLES;
 const User = db.user;
 
-const checkDuplicateUsernameOrEmail = (req, res, next) => {
-  User.findOne({ username: req.body.username }).then((user) => {
-    if (user) {
-      return res.status(400).send({ message: "Failed! Username is already in use!" });
-    }
+checkDuplicateUsernameOrEmail = (req, res, next) => {
+    User.findOne({
+        username: req.body.username
+    }).then(user => {
+        if (user) {
+            res.status(400).send({ message: "Failed! Username is already in use!" });
+            return;
+        }
 
-    User.findOne({ email: req.body.email }).then((user) => {
-      if (user) {
-        return res.status(400).send({ message: "Failed! Email is already in use!" });
-      }
+        User.findOne({
+            email: req.body.email
+        }).then(user => {
+            if (user) {
+                res.status(400).send({ message: "Failed! Email is already in use!" });
+                return;
+            }
 
-      next();
-    }).catch((err) => {
-      res.status(500).send({ message: err });
+            next();
+        }).catch(err => {
+            res.status(500).send({ message: err });
+        });
+    }).catch(err => {
+        res.status(500).send({ message: err });
     });
-  }).catch((err) => {
-    res.status(500).send({ message: err });
-  });
 };
+
 
 const checkRolesExisted = (req, res, next) => {
   if (req.body.roles) {
